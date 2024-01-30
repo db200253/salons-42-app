7de0320591ebd8b5d20c0f1c491d2b52384a5285
db200253 <duvieu.baptistepro@gmail.com>
2024-01-30 17:16:10 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -13,18 +13,24 @@ const App = () => {
 
   return (
     <div className="app">
-      <Navbar />
-      <CodeEditor onCodeChange={handleCodeChange} />
-      <div className="preview">
-        {/* Afficher ici la zone de rendu en direct */}
-        <h2>Rendu en Direct :</h2>
-        {previewCode !== null ? (
-          <div dangerouslySetInnerHTML={{ __html: previewCode }} />
-        ) : (
-          <p>Aucun code à afficher pour le moment.</p>
-        )}
-      </div>
-      <Footer />
+	<div>
+	    <Navbar />
+	</div>
+	<div class="ce">
+	    <CodeEditor onCodeChange={handleCodeChange} />
+	    <div className="preview">
+		{/* Afficher ici la zone de rendu en direct */}
+		<h2>Rendu en Direct :</h2>
+		{previewCode !== null ? (
+		    <div dangerouslySetInnerHTML={{ __html: previewCode }} />
+		) : (
+		    <p>Aucun code à afficher pour le moment.</p>
+		)}
+	    </div>
+	</div>
+	<div>
+	    <Footer />
+	</div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+  const [userName, setUserName] = useState('');
+  const [showUserName, setShowUserName] = useState(false);
+
+  const handleTutorialClick = () => {
+    // Logique pour lancer le tutoriel
+    console.log('Lancer le tutoriel');
+  };
+
+  const handleResetClick = () => {
+    // Logique pour reset
+    console.log('Reset');
+    setUserName('');
+    setShowUserName(false);
+  };
+
+  const handleEnterName = () => {
+    const name = prompt('Entrez votre nom :');
+    if (name) {
+      setUserName(name);
+      setShowUserName(true);
+    }
+  };
+
   return (
-    <nav>
-      {/* Votre contenu de la navbar ici */}
-    </nav>
+    <header class="App-header">
+	<div className="logo">Logo</div>
+	<button onClick={handleTutorialClick}>Lancer le Tutoriel</button>
+	<button onClick={handleResetClick}>Reset</button>
+	<button onClick={handleEnterName}>Entrer le Nom</button>
+	{showUserName && <div className="user-name">Bonjour, {userName} !</div>}
+    </header>
   );
 };
 
